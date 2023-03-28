eac1fd66cd13a2f0ef1e3a0b57085ccf0b21d7ad
konigorikun <konishitech@gmail.com>
2023-03-28 17:58:33 +0900

    新規登録機能実装（バリデーションの設定）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  
+  validates :nickname, presence: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :date_of_birthday, presence: true
 end
