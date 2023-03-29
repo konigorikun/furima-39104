5f1b0f793db0198156e90205cd85ac4b98ecd086
konigorikun <konishitech@gmail.com>
2023-03-29 12:35:07 +0900

    バリデーションとHTMLの一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,6 @@ class User < ApplicationRecord
   validates :firstname_kana, presence: true
   validates :lastname_kana, presence: true
   validates :date_of_birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
