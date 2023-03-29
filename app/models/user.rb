0f09be2195e13b7755edb3d70673a067ee06b43d
konigorikun <konishitech@gmail.com>
2023-03-29 15:38:08 +0900

    バリデーション、コメントアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,18 @@ class User < ApplicationRecord
   
   
   validates :nickname, presence: true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :firstname_kana, presence: true
-  validates :lastname_kana, presence: true
   validates :date_of_birthday, presence: true
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :firstname
+    validates :lastname
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :firstname_kana
+    validates :lastname_kana
+  end
 end
