e9aafec6941e654a4f9a3c04c9d3a034b85fc38c
konigorikun <konishitech@gmail.com>
2023-03-30 10:54:22 +0900

    バリデーション設定及び各モデル内の記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :detail, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_from_organization_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :cost, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                     presence: { message: "can't be blank "}
+  end
+  
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  has_many :items
   
   validates :nickname, presence: true
   validates :date_of_birthday, presence: true
