c26b2545f090d32b0a1070e8fd1c2e5a018ee0cd
konigorikun <konishitech@gmail.com>
2023-04-07 14:35:47 +0900

    フォームの実装と保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_params)
     if @order_shipping_address.valid?
-      @order_shipping_address.save
-      redirect_to root_path
+       @order_shipping_address.save
+       redirect_to root_path
     else
-      render :new
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:post_number, :ship_from_organization_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping_address).permit(:post_number, :ship_from_organization_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
